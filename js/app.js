3a4a2849ec0f458de76dbcd0371b62db9a1e5f78
j-chaidez <jonathan_chaidez@outlook.com>
2016-07-22 15:53:11 -0700

    beta
    test

--- js/app.js
+++ js/app.js
@@ -167,16 +167,25 @@ var app = (function(_) {
 			}
 		},
 		
+		// executeTurn is responsible for all of the turn logic
 		executeTurn: function(element) {
+			// if the element's class name is not equal to clicked
 			if (!element.className.match(/clicked/g)) {
 				
+				// check the current symbol
 				if (current.symbolPicture[6] === 'x') {
+					// get the player1 class element, set it to active
 					document.getElementById("player1").className = "players" + " " + "active";
+					// get the player2 class element, reset default class values
 					document.getElementById("player2").className = "players";
+					// set the appropriate class'
 					element.className += " " + "box-filled-2" + " " + "clicked";
+					// set the background image element
 					element.style.backgroundImage = "url(" + current.symbolPicture + ")";
+					// pop the move from the moves array
 					this.popMove(element.id);
 				} else {
+					// do the exact same thing here, only in reverse
 					document.getElementById("player2").className = "players" + " " + "active";
 					document.getElementById("player1").className = "players";
 					element.className += " " + "box-filled-1" + " " + "clicked";
@@ -184,75 +193,107 @@ var app = (function(_) {
 					this.popMove(element.id);
 				}
 				
+				// increment filled by one
 				filled += 1;
 				
-				element.style.backgroundImage = "url(" + current.symbolPicture;
-				
+				// set the background image for the element
+				element.style.backgroundImage = "url(" + current.symbolPicture + ")";
+				// switch the player's turns
 				if (current === playerOne) {
+					// if player one has the turn currently, switch to two
 					current = playerTwo;
+					// check to see if the second player is an AI player
 					if (current.ai) {
 						try {
+							// if so, execute the computer move
 							app.executeComputerMove(element.id);
 						} catch (err) {};
 					}
 				} else {
+					// otherwise, it's
 					current = playerOne;
 				}
 				
 			}
 		},
 		
+		// executeComputerMove is responsible 
 		executeComputerMove: function(last) {
+			// get a list of available moves
 			var availableMove = app.getAvailableMoves(last);
+			// log for debugging purposes
 			console.log(availableMove);
+			// get a random element of the array
 			var randomArray = Math.floor(Math.random() * availableMove.length);
+			// get a random element from within the randomly selected array 
 			var randomElement = Math.floor(Math.random() * availableMove[randomArray].length);
 			try {
+				// get an element 
 				var element = availableMove[randomArray][randomElement]
+				// click that element to trigger the computer move 
 				document.getElementById(availableMove[randomArray][randomElement]).click();
 			} catch (err) {
+				// catch any errors that may occur
 				console.log(err);
 			};
 		},
 		
+		// get available moves gets the 'best' move for the computer 
 		getAvailableMoves: function(last) {
+			// declare a local array 
 			var n = [];
+			// set an array that will be used as the default length for the upcoming for loop
 			var lowest = [1, 1, 1];
+			// loop through the winninCombinationsOne array
 			for (var j = 0; j < winningCombinationsOne.length; j++) {
+				// log for debugging purposes
 				console.log(winningCombinationsTwo[j].length);
+				// if winningCombinationsTwo[j]'s length is equal to one, and the availablesMoves[j]'s length is greater than zero, then the computer can win, return the move
 				if (winningCombinationsTwo[j].length === 1 && availableMoves[j].length > 0) {
 					return availableMoves[j];
 				}
+				// if winningCombinationsOne[j]'s length is less than or equal to the lowest's length, and available moves length is greater than zero
 				if (winningCombinationsOne[j].length <= lowest.length && availableMoves[j].length > 0) {
+					// loop through winningCombinationsTwo[j]'s array
 					for (var i = 0; i < winningCombinationsTwo[j].length; i++) {
+						// if the length is equal to one, then the computer should make a block move by choosing winningCombinationsOne[j]
 						if (winningCombinationsOne[j].length === 1) {
 							return availableMoves[j];
 						} else {
+						// else we just push the nested ararys into n
 							n.push(availableMoves[j]);
 						}
 					}
 				}
 			}
+			// return n
 			return n;
 		},
 		
+		// popMove is responsible for slicing out available moves so they can't be made twice
 		popMove: function(id) {
-			
+			// declare an array that will be used as the target
 			var targetArray;
+			// check the current symbol
 			if (current.symbolPicture[6] === 'x') {
+				// set the appropriate array 
 				targetArray = winningCombinationsOne;
 			} else {
 				targetArray = winningCombinationsTwo;
 			}
 			
+			// loop over the target array
 			for (var i = 0; i < targetArray.length; i++) {
+				// loop over the elements of that array 
 				for (var j = 0; j < targetArray[i].length; j++) {
+					// splice the move that was just made
 					if (targetArray[i][j] === id) {
 						targetArray[i].splice(j, 1);
 					}
 				}
 			}
 			
+			// do the exact same thing for available moves
 			for (var i = 0; i < availableMoves.length; i++) {
 				for (var j = 0; j < availableMoves[i].length; j++) {
 					if (availableMoves[i][j] === id) {
@@ -260,36 +301,51 @@ var app = (function(_) {
 					}
 				}
 			}
-			
+			// check for a winner
 			this.checkForWinner();
 		},
 		
+		// check for winner is responsible for checking for a winner
 		checkForWinner: function() {
+			// declare a target array
 			var targetArray; 
+			// check the current player
 			if (current.symbolPicture[6] === 'x') {
+				// select the appropriate array 
 				targetArray = winningCombinationsOne;
 			} else {
 				targetArray = winningCombinationsTwo;
 			}
+			// loop through the target array 
 			for (var i = 0; i < targetArray.length; i++) {
+				// if the target array's length is less than or equal to zero 
 				if (targetArray[i].length <= 0) {
+					// that means someone has won, show the game over page 
 					body.innerHTML = htmlSnippets.gameOver;
+					// show who won!
 					document.getElementsByClassName("message")[0].innerHTML = current.name + " wins!";
+					// add the loadstart function to the button 
 					document.getElementsByClassName("button")[0].addEventListener("click", app.loadStart, false);
+					// check the current player's symbol
 					if (current.symbolPicture[6] === 'x') {
+						// get the screen class
 						var screen = document.getElementsByClassName("screen")[0];
+						// get the message class
 						var message = document.getElementsByClassName("message")[0];
+						// style the background color and image 
 						screen.style.backgroundColor = "#3688C3";
-						screen.style.backgroundImage = "url(img/" + current.symbolPicture[6] + "_win.svg";
+						screen.style.backgroundImage = "url(img/" + current.symbolPicture[6] + "_win.svg" + ")";
 						screen.style.backgroundRepeat = "no-repeat";
 						screen.style.backgroundPosition = "center";
 						screen.style.backgroundSize = "200px";
+						// reset the game 
 						this.resetGame();
 					} else {
+						// do the exact same thing only for O
 						var screen = document.getElementsByClassName("screen")[0];
 						var message = document.getElementsByClassName("message")[0];
 						screen.style.backgroundColor = "#FFA000";
-						screen.style.backgroundImage = "url(img/" + current.symbolPicture[6] + "_win.svg";
+						screen.style.backgroundImage = "url(img/" + current.symbolPicture[6] + "_win.svg" + ")";
 						screen.style.backgroundRepeat = "no-repeat";
 						screen.style.backgroundPosition = "center";
 						screen.style.backgroundSize = "200px";
@@ -297,14 +353,19 @@ var app = (function(_) {
 					}
 				}
 			}
+			// check for a tie by checking how many squares are filled
 			if (filled === 9) {
+				// show the gameOver snippet
 				body.innerHTML = htmlSnippets.gameOver;
 				document.getElementsByClassName("message")[0].innerHTML = "It's a draw!";
+				// reset the game
 				this.resetGame();
 				document.getElementsByClassName("button")[0].addEventListener("click", app.loadStart, false);
 			}
 		},
 		
+		
+		// reset the game by setting all variables back to their defaults
 		resetGame: function() {
 			filled = 0;
 			current = playerOne;
@@ -322,4 +383,5 @@ var app = (function(_) {
 		
 	}
 }(Core));
+// load the program on initialize
 app.loadStart();
